e05cef8cac20d155f6a405f735b1b15a277ba066
Peter Yates <89813543+PNRYates@users.noreply.github.com>
2023-11-14 19:06:24 +1100

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://www.peteryates.net", // replace this with your deployed domain
+  author: "Peter Yates",
+  desc: "My website",
+  title: "Peter Yates",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
